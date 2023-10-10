8018b4f1b1a0f488eef6dec60be89b2d23196faf
Felipe-RA <49454068+Felipe-RA@users.noreply.github.com>
2023-10-10 01:43:53 -0500

    making code extra smelly

---
+++ smelly_code.py
@@ -0,0 +1,23 @@
+print("hi")
+
+def imEvil():
+    if True:
+        print("aaaaaaaaaa")
+        if True:
+            print("aaaaaaaaaa")
+            if True:
+                print("aaaaaaaaaa")
+                if True:
+                    print("aaaaaaaaaa")
+                    if True:  # im funny
+                        print("aaaaaaaaaa")
+                        if True:
+                            print("aaaaaaaaaa")
+                            if True:
+                                print("aaaaaaaaaa")
+                                i = 0
+                                while True:
+                                    i += 1
+                                    print("bbbbbbb")
+                                    if i == 100:
+                                        print("hehehe")
