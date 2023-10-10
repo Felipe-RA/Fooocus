815b2f751dcb323ad82bc55fc6ddc93436da31df
FelipeRodriguezA <felipeangel50@gmail.com>
2023-10-10 01:57:53 -0500

    changing again my dummy test

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -6,8 +6,9 @@ def import_all_modules(root_folder):
     for root, dirs, files in os.walk(root_folder):
         for file in files:
             if file.endswith('.py'):
-                module = os.path.join(root, file).replace('/', '.').replace('.py', '')
-                importlib.import_module(module)
+                module_path = os.path.join(root, file).replace('/', '.').replace('.py', '')
+                if not module_path.startswith('.'):
+                    importlib.import_module(module_path)
 
 def test_dummy():
     import_all_modules('.')
